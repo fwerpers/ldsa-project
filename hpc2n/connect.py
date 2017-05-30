2a1f68ded85d6b6755d1d1cb62155d0fb1e04862
Felix Werpers <felix@werpers.com>
2017-05-30 14:55:10 +0200

    Add script to connect to master node via SSH

---
+++ hpc2n/connect.py
@@ -0,0 +1,21 @@
+import subprocess
+import json
+
+# stack_name = ''
+#
+# with open('inventory.json', 'r') as f:
+#     stack_info = json.load(f)
+#     if 'stack_name' in a:
+#         stack_name = stack_info['stack_name']
+
+stack_name = 'test'
+json_data = subprocess.Popen("openstack stack output show -f json --all " + stack_name, shell=True, stdout=subprocess.PIPE).stdout.read()
+
+stack_info = json.loads(json_data)
+ip = stack_info['hadoop_master_public_ip']
+ip = json.loads(ip)
+ip = ip['output_value']
+
+args = ['ssh', 'ubuntu@' + ip, '-i', 'hpc2n_key.pem']
+print(args)
+subprocess.call(args)
