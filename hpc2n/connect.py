705fc6322be5fa7d5be4958b7ab45cd330001bbb
Felix Werpers <felix@werpers.com>
2017-05-31 22:27:07 +0200

    Make parsing of stack info cleaner

--- hpc2n/connect.py
+++ hpc2n/connect.py
@@ -8,14 +8,30 @@ import json
 #     if 'stack_name' in a:
 #         stack_name = stack_info['stack_name']
 
+def parse_stack_info(string):
+
+    def parse(d):
+        for key in d.keys():
+            try:
+                next_d = json.loads(d[key])
+                val = parse(next_d)
+            except TypeError:
+                continue
+            except ValueError:
+                val = d[key]
+            d[key] = val
+
+        return(d)
+
+    base_dict = json.loads(string)
+    return(parse(base_dict))
+
+
 stack_name = 'test'
 json_data = subprocess.Popen("openstack stack output show -f json --all " + stack_name, shell=True, stdout=subprocess.PIPE).stdout.read()
+stack_info = parse_stack_info(json_data)
 
-stack_info = json.loads(json_data)
-ip = stack_info['hadoop_master_public_ip']
-ip = json.loads(ip)
-ip = ip['output_value']
-
+ip = stack_info.get('hadoop_master_public_ip').get('output_value')
 args = ['ssh', 'ubuntu@' + ip, '-i', 'hpc2n_key.pem']
 print(args)
 subprocess.call(args)
