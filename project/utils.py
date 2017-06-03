c7b555643cb83ad969063e35b63f082e6339af29
Felix Werpers <felix@werpers.com>
2017-06-03 13:45:54 +0200

    Add stack info parser

--- project/utils.py
+++ project/utils.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 def get_stack_name():
     with open('stack_name', 'r') as f:
@@ -14,3 +15,21 @@ def get_key_path():
     region_dir = os.environ['OS_REGION_NAME']
     key_path = os.path.join(region_dir, region_dir.lower() + '_key.pem')
     return(os.path.abspath(key_path))
+
+def parse_stack_info(string):
+
+    def parse(d):
+        for key in d.keys():
+            try:
+                next_d = json.loads(d[key])
+                val = parse(next_d)
+            except TypeError:
+                continue
+            except ValueError:
+                val = d[key]
+            d[key] = val
+
+        return(d)
+
+    base_dict = json.loads(string)
+    return(parse(base_dict))
